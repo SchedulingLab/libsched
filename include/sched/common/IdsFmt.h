22356441ec1207713dc15e4b6598f25f34893230
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-03-23 16:03:28 +0100

    add InputFmt

--- include/sched/common/IdsFmt.h
+++ include/sched/common/IdsFmt.h
@@ -7,25 +7,31 @@
 
 template <>
 struct fmt::formatter<sched::MachineId> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.end()) { return ctx.end(); }
+
   template <typename FormatContext>
-  auto format(sched::MachineId id, FormatContext& ctx) -> decltype(ctx.out()) {
+  auto format(sched::MachineId id, FormatContext& ctx) const -> decltype(ctx.out()) {
     return format_to(ctx.out(), "{}", static_cast<std::size_t>(id));
   }
 };
 
 template <>
 struct fmt::formatter<sched::JobId> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.end()) { return ctx.end(); }
+
   template <typename FormatContext>
-  auto format(sched::JobId id, FormatContext& ctx) -> decltype(ctx.out()) {
+  auto format(sched::JobId id, FormatContext& ctx) const -> decltype(ctx.out()) {
     return format_to(ctx.out(), "{}", static_cast<std::size_t>(id));
   }
 };
 
 template <>
 struct fmt::formatter<sched::OperationId> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.end()) { return ctx.end(); }
+
   template <typename FormatContext>
-  auto format(sched::OperationId id, FormatContext& ctx) -> decltype(ctx.out()) {
-    return format_to(ctx.out(), "({},{})", id.job, id.index);
+  auto format(sched::OperationId id, FormatContext& ctx) const -> decltype(ctx.out()) {
+    return format_to(ctx.out(), "({}, {})", id.job, id.index);
   }
 };
 
---
+++ include/sched/shop/InputFmt.h
@@ -0,0 +1,50 @@
+#ifndef SCHED_SHOP_INPUT_FMT_H
+#define SCHED_SHOP_INPUT_FMT_H
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <sched/common/IdsFmt.h>
+
+#include "JobListInput.h"
+#include "MachineListInput.h"
+#include "OperationListInput.h"
+
+template <>
+struct fmt::formatter<sched::shop::JobListInput> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.end()) { return ctx.end(); }
+
+  template <typename FormatContext>
+  auto format(const sched::shop::JobListInput& input, FormatContext& ctx) const -> decltype(ctx.out()) {
+    format_to(ctx.out(), "[");
+    format_to(ctx.out(), "{}", fmt::join(input, ", "));
+    return format_to(ctx.out(), "]");
+  }
+};
+
+template <>
+struct fmt::formatter<sched::shop::OperationListInput> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.end()) { return ctx.end(); }
+
+  template <typename FormatContext>
+  auto format(const sched::shop::OperationListInput& input, FormatContext& ctx) const -> decltype(ctx.out()) {
+    format_to(ctx.out(), "[");
+    format_to(ctx.out(), "{}", fmt::join(input, ", "));
+    return format_to(ctx.out(), "]");
+  }
+};
+
+template <>
+struct fmt::formatter<sched::shop::MachineListInput> {
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.end()) { return ctx.end(); }
+
+  template <typename FormatContext>
+  auto format(const sched::shop::MachineListInput& input, FormatContext& ctx) const -> decltype(ctx.out()) {
+    std::size_t i = 0;
+    format_to(ctx.out(), "[");
+    format_to(ctx.out(), "{}", fmt::join(input, "; "));
+    return format_to(ctx.out(), "]");
+  }
+};
+
+#endif // SCHED_SHOP_INPUT_FMT_H
