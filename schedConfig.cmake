9270999ed2ef8b39bee74ec77bdc765714bffc2f
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-04-16 20:32:06 +0200

    add ParallelOptimalAlgorithm

--- schedConfig.cmake
+++ schedConfig.cmake
@@ -1,17 +1,17 @@
 include (CMakeFindDependencyMacro)
 
 get_filename_component (SCHED_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
-include ("${SCHED_CMAKE_DIR}/schedTargets.cmake")
+include("${SCHED_CMAKE_DIR}/schedTargets.cmake")
 
 find_dependency(Threads)
 find_dependency(fmt)
 
-if (NOT BUILD_SHARED_LIBS)
+if(NOT BUILD_SHARED_LIBS)
   find_dependency(nlohmann_json)
 endif ()
 
 set(SCHED_FOUND TRUE)
 
-include ("${SCHED_CMAKE_DIR}/schedConfigVersion.cmake")
+include("${SCHED_CMAKE_DIR}/schedConfigVersion.cmake")
 
 message(STATUS "Found sched (version \"${PACKAGE_VERSION}\") in ${CMAKE_CURRENT_LIST_DIR}")
