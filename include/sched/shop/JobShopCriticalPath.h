640e05fd2459566341925bc27bb27d804562d0fe
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-04-20 19:03:20 +0200

    add missing SCHED_API

--- include/sched/shop/JobShopCriticalPath.h
+++ include/sched/shop/JobShopCriticalPath.h
@@ -1,11 +1,13 @@
 #ifndef SCHED_SHOP_JOB_SHOP_CRITICAL_PATH_H
 #define SCHED_SHOP_JOB_SHOP_CRITICAL_PATH_H
 
+#include <sched/common/Api.h>
+
 #include "JobShopSchedule.h"
 
 namespace sched::shop {
 
-  std::vector<JobShopTask> critical_path(const JobShopSchedule& schedule);
+  SCHED_API std::vector<JobShopTask> critical_path(const JobShopSchedule& schedule);
 
 }
 
