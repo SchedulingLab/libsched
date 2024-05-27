c44d4a92ba66719936f7f85139eae4704a0b61ec
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-05-27 14:12:44 +0200

    fix Solution

--- include/sched/shop/Solution.h
+++ include/sched/shop/Solution.h
@@ -1,14 +1,18 @@
 #ifndef SCHED_SHOP_SOLUTION_H
 #define SCHED_SHOP_SOLUTION_H
 
+#include <optional>
 #include <utility>
+
+#include "EngineConcepts.h"
 
 namespace sched::shop {
 
   template<typename Engine, typename Criterion, typename Instance>
+    requires(concepts::EngineFor<Engine, Instance>)
   struct Solution {
     using Input = typename Engine::Input;
-    using Schedule = decltype(std::declval<Engine>()(std::declval<Instance>(), std::declval<Input>()));
+    using Schedule = std::optional<typename Engine::Schedule>;
     using Fitness = decltype(std::declval<Criterion>()(std::declval<Instance>(), *std::declval<Schedule>()));
 
     Input input;
