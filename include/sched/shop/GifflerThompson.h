5e4ea16cb0f35361bb5cbb50711fb840e10f60e8
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-05-28 12:35:02 +0200

    improve GT

--- include/sched/shop/GifflerThompson.h
+++ include/sched/shop/GifflerThompson.h
@@ -16,33 +16,28 @@ namespace sched::shop {
     JobShopStates<Instance> states(instance);
     JobShopSchedule schedule;
 
-    for (;;) {
-      std::vector<OperationId> available_operations;
+    std::vector<JobShopTask> available_tasks;
 
-      for (auto job : jobs(instance)) {
-        if (states.has_next_operation(job)) {
-          available_operations.push_back(states.next_operation(job));
-        }
-      }
-
-      if (available_operations.empty()) {
-        break;
-      }
-
-      std::vector<JobShopTask> tasks;
-
-      for (auto operation : available_operations) {
+    auto add_next_operation_if_available = [&states,&instance,&available_tasks](JobId job) {
+      if (states.has_next_operation(job)) {
+        auto next_operation = states.next_operation(job);
         MachineId machine = instance.assigned_machine_for_operation(operation);
-        tasks.push_back(states.create_task(operation, machine));
+        available_tasks.push_back(states.create_task(operation, machine));
       }
+    };
 
-      const JobShopTask earliest_completed_task = *std::min_element(tasks.begin(), tasks.end(), [](const JobShopTask& lhs, const JobShopTask& rhs) {
+    for (auto job : jobs(instance)) {
+      add_next_operation_if_available(job);
+    }
+
+    while (!available_tasks.empty()) {
+      const JobShopTask earliest_completed_task = *std::min_element(available_tasks.begin(), available_tasks.end(), [](const JobShopTask& lhs, const JobShopTask& rhs) {
         return lhs.completion < rhs.completion;
       });
 
       std::vector<JobShopTask> conflicting_tasks;
 
-      for (const auto& task : tasks) {
+      for (const auto& task : available_tasks) {
         if (task.operation == earliest_completed_task.operation) {
           continue;
         }
@@ -52,13 +47,19 @@ namespace sched::shop {
         }
       }
 
-      if (conflicting_tasks.empty()) {
-        states.update_schedule(earliest_completed_task, schedule);
-        continue;
-      }
-
-      const JobShopTask chosen_task = *std::min_element(conflicting_tasks.begin(), conflicting_tasks.end(), compare);
+      const JobShopTask chosen_task = conflicting_tasks.empty() ? earliest_completed_task : *std::min_element(conflicting_tasks.begin(), conflicting_tasks.end(), compare);
       states.update_schedule(chosen_task, schedule);
+
+      auto chosen_iterator = std::find_if(available_tasks.begin(), available_tasks.end(), [&chosen_task](const JobShopTask& task) {
+        return task.operation == chosen_task.operation;
+      });
+
+      // remove chosen task from the available tasks
+      auto last_iterator = --available_tasks.end();
+      std::iter_swap(chosen_iterator, last_iterator);
+      available_tasks.pop_back();
+
+      add_next_operation_if_available(chosen_task.operation.job);
     }
 
     return schedule;
