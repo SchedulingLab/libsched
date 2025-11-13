56896e6bebba37f3739afc4864ffc382995cd643
Julien Bernard <julien.bernard@univ-fcomte.fr>
2025-11-13 13:59:19 +0100

    add missing SCHED_API

--- include/sched/shop/input/HoistInputConversion.h
+++ include/sched/shop/input/HoistInputConversion.h
@@ -3,14 +3,15 @@
 #ifndef SCHED_SHOP_HOIST_INPUT_CONVERSION_H
 #define SCHED_SHOP_HOIST_INPUT_CONVERSION_H
 
+#include <sched/Api.h>
 #include <sched/shop/input/HoistEmptyInput.h>
 #include <sched/shop/input/HoistLoadedInput.h>
 #include <sched/shop/instance/HoistSchedulingInstance.h>
 
 namespace sched::shop {
 
-  HoistEmptyInput to_empty_input(const HoistLoadedInput& loaded_input, const HoistSchedulingInstance& instance);
-  HoistLoadedInput to_loaded_input(const HoistEmptyInput& empty_input, const HoistSchedulingInstance& instance);
+  SCHED_API HoistEmptyInput to_empty_input(const HoistLoadedInput& loaded_input, const HoistSchedulingInstance& instance);
+  SCHED_API HoistLoadedInput to_loaded_input(const HoistEmptyInput& empty_input, const HoistSchedulingInstance& instance);
 
 }
 
