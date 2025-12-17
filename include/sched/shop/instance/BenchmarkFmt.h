d5e97d42030b2ed5a1759cd4b24a0a534946a848
Julien Bernard <julien.bernard@univ-fcomte.fr>
2025-12-17 17:34:33 +0100

    add fmt helper for benchmark classes

---
+++ include/sched/shop/instance/BenchmarkFmt.h
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2022-2025 Julien Bernard
+#ifndef SCHED_SHOP_BENCHMARK_FMT_H
+#define SCHED_SHOP_BENCHMARK_FMT_H
+
+#include <sched/shop/instance/JobShopBenchmark.h>
+#include <sched/shop/instance/FlexibleJobShopBenchmark.h>
+#include <sched/support/FmtHelper.h>
+
+template<>
+struct std::formatter<sched::shop::JobShopBenchmark> : std::formatter<std::string>
+{
+  // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+  template<typename Context>
+  auto format(const sched::shop::JobShopBenchmark& benchmark, Context& ctx) const -> Context::iterator
+  {
+    return std::formatter<std::string>::format(benchmark.name, ctx);
+  }
+};
+
+template<>
+struct std::formatter<sched::shop::FlexibleJobShopBenchmark> : std::formatter<std::string>
+{
+  // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+  template<typename Context>
+  auto format(const sched::shop::FlexibleJobShopBenchmark& benchmark, Context& ctx) const -> Context::iterator
+  {
+    return std::formatter<std::string>::format(benchmark.name, ctx);
+  }
+};
+
+
+#endif // SCHED_SHOP_BENCHMARK_FMT_H
